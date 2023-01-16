16ce82274abeed3a7f25ffac704da2ba3e292b98
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-01-16 19:24:41 +0530

    optimize memory usage with spans

--- Program.cs
+++ Program.cs
@@ -16,31 +16,33 @@ bms.ConnectionStatusChanged += (object _, ConnectionStatusChangedEventArgs e) =>
     Console.WriteLine($"CONNECTED: {e.Connected}");
 };
 
-bms.MessageReceived += async (object _, MessageReceivedEventArgs e) =>
+bms.MessageReceived += (object _, MessageReceivedEventArgs evnt) =>
 {
     Console.Clear();
 
-    if (!e.Data.IsValid())
+    var data = evnt.Data.AsSpan();
+
+    if (!data.IsValid())
     {
         Console.WriteLine("invalid data received!");
-        await Task.Delay(pollFrequencyMillis);
+        Thread.Sleep(pollFrequencyMillis);
         bms.QueryData();
         return;
     }
 
-    var response = e.Data[11..]; //skip the first 10 bytes
-    var cellCount = response[1] / 3; //pos 1 is total cell bytes length. 3 bytes per cell.
+    var res = data[11..]; //skip the first 10 bytes
+    var cellCount = res[1] / 3; //pos 1 is total cell bytes length. 3 bytes per cell.
 
     Console.WriteLine($"cell count:{cellCount}");
 
     ushort pos = 0;
-    for (byte i = 1; pos <= response.Length - 2 && i <= cellCount; i++)
+    for (byte i = 1; i <= cellCount; i++)
     {
         //cell voltage groups (of 3 bytes) start at pos 2
         //first cell voltage starts at position 3 (pos 2 is cell number). voltage value is next 2 bytes.
         // ex: .....,1,X,X,2,Y,Y,3,Z,Z
         //pos is increased by 3 bytes in order to skip the address/code byte
-        cellVoltages[i] = response.Read2Bytes(pos += 3) / 1000f;
+        cellVoltages[i] = res.Read2Bytes(pos += 3) / 1000f;
 
         Console.WriteLine($"cell {i}: {cellVoltages[i]:0.000} V");
     }
@@ -54,15 +56,15 @@ bms.MessageReceived += async (object _, MessageReceivedEventArgs e) =>
     Console.WriteLine($"max cell: [{maxCell.Key}] {maxCell.Value:0.000} V");
     Console.WriteLine($"cell diff: {cellDiff:0.000} V");
 
-    var mosTemp = response.Read2Bytes(pos += 3);
-    var probe1Temp = response.Read2Bytes(pos += 3);
-    var probe2Temp = response.Read2Bytes(pos += 3);
+    var mosTemp = res.Read2Bytes(pos += 3);
+    var probe1Temp = res.Read2Bytes(pos += 3);
+    var probe2Temp = res.Read2Bytes(pos += 3);
     Console.WriteLine($"mos temp: {mosTemp} C | t1: {probe1Temp} C | t2: {probe2Temp} C");
 
-    var packVoltage = response.Read2Bytes(pos += 3) / 100f;
+    var packVoltage = res.Read2Bytes(pos += 3) / 100f;
     Console.WriteLine($"pack voltage: {packVoltage:00.00} V");
 
-    var rawVal = response.Read2Bytes(pos += 3);
+    var rawVal = res.Read2Bytes(pos += 3);
     var isCharging = Convert.ToBoolean(int.Parse(Convert.ToString(rawVal, 2).PadLeft(16, '0')[..1])); //pick first bit of padded 16 bit binary representation and turn it in to a bool
     Console.WriteLine($"charging: {isCharging}");
 
@@ -72,13 +74,13 @@ bms.MessageReceived += async (object _, MessageReceivedEventArgs e) =>
     var avgCurrentAmps = recentAmpReadings.GetAverage();
     Console.WriteLine($"current: {avgCurrentAmps:0.0} A");
 
-    var capacityPct = Convert.ToUInt16(response[pos += 3]);
+    var capacityPct = Convert.ToUInt16(res[pos += 3]);
     Console.WriteLine($"capacity: {capacityPct} %");
 
-    var isWarning = response.Read2Bytes(pos += 15) > 0;
+    var isWarning = res.Read2Bytes(pos += 15) > 0;
     Console.WriteLine($"warning: {isWarning}");
 
-    var packCapacity = response.Read4Bytes(pos += 88);
+    var packCapacity = res.Read4Bytes(pos += 88);
     Console.WriteLine($"pack capacity: {packCapacity} Ah");
 
     var availableCapacity = packCapacity / 100f * capacityPct;
@@ -101,7 +103,7 @@ bms.MessageReceived += async (object _, MessageReceivedEventArgs e) =>
     var cRate = Math.Round(avgCurrentAmps / packCapacity, 2, MidpointRounding.AwayFromZero);
     Console.WriteLine($"c-rate: {cRate}");
 
-    await Task.Delay(pollFrequencyMillis);
+    Thread.Sleep(pollFrequencyMillis);
     bms.QueryData();
 };
 
@@ -109,37 +111,39 @@ bms.Connect();
 
 public static class Extensions
 {
-    const string commandHex = "4E5700130000000006030000000000006800000129";
+    private static readonly byte[] cmd = Convert.FromHexString("4E5700130000000006030000000000006800000129");
 
     public static void QueryData(this SerialPortInput port)
     {
-        port.SendMessage(Convert.FromHexString(commandHex));
+        port.SendMessage(cmd);
     }
 
-    public static ushort Read2Bytes(this byte[] input, ushort startPos)
+    public static ushort Read2Bytes(this Span<byte> data, ushort startPos)
     {
-        var hex = Convert.ToHexString(input, startPos, 2);
+        var hex = Convert.ToHexString(data.Slice(startPos, 2));
         return ushort.Parse(hex, NumberStyles.HexNumber);
     }
 
-    public static uint Read4Bytes(this byte[] input, ushort startPos)
+    public static uint Read4Bytes(this Span<byte> input, ushort startPos)
     {
-        var hex = Convert.ToHexString(input, startPos, 4);
+        var hex = Convert.ToHexString(input.Slice(startPos, 4));
         return uint.Parse(hex, NumberStyles.HexNumber);
     }
 
-    public static bool IsValid(this byte[] data)
+    public static bool IsValid(this Span<byte> data)
     {
         if (data.Length < 8)
             return false;
 
-        var header = Convert.ToHexString(data, 0, 2);
+        var header = Convert.ToHexString(data[..2]); //get hex from first 2 bytes
 
         if (header is not "4E57")
             return false;
 
-        //sum up all bytes excluding the last 4 bytes
-        var crc_calc = data[0..^3].Sum(Convert.ToInt32);
+        short crc_calc = 0;
+
+        foreach (var b in data[0..^3])//sum up all bytes excluding the last 4 bytes
+            crc_calc += b;
 
         //convert last 2 bytes to hex and get short from that hex
         var crc_lo = data[^2..^0].Read2Bytes(0);
